c766b6985c9c193668364374bd1ed0ce770b6039
KALYAN GADDE <kalyangadde@Magnus-MacBook-Pro.local>
2025-03-22 09:21:42 -0400

    updating code

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,7 @@
+# tests/conftest.py
+
 import sys
 import os
 
-# Add src/ to Python path so it can be imported
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "src")))
+# Dynamically add the 'src' directory to Python's import path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
